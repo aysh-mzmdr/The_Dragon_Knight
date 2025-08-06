ea4f6869d7a1250a791fcadb30aae3e476296593
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2025-08-06 17:26:18 +0530

    Published Chapter 1

---
+++ src/Chapters/Chapter1.jsx
@@ -0,0 +1,90 @@
+import Chapter from "../Chapter.jsx"
+
+function Chapter1(){
+
+    const story=(
+        <>
+            <p>Long time ago, the world was quite different from what it's now. Had someone made the world map at that time, then you might mistake it to be of some alien planet. There was no Indian Subcontinent or the Americas; the tectonic plates were at completely different locations from where they are now. At that time, there were no countries, but rather kingdoms that ruled different parts of the world. One such powerful kingdom was Vaibhajya Dynasty. In the eastern coast of the dynasty, there was the town of Ajaynagar. It was a fairly prosperous town, which focused heavily on the trade of wheat and jute. The climate here was the most favourable in the entire dynasty for growing such crops. Amidst the traders and farmers, every town and village had several warrior families. All male members of the warrior families were trained from the early childhood to serve the army. They were supposed to join the army at the age of 16, and were alloted sword and shield as symbol of them joining the army. However, they stayed within the kingdom, and were meant for defending the land. Only once they turned 18 were they allowed to go out on wars. The females of the warrior families were also given training from childhood, but were supposed to join the army only after 18, and had to retire after the age of 30 to look after the family. That didn't mean the end of their duty as a warrior. They still had the responsibility of defending their houses at the time of war, when the males were supposed to be fighting on the alien land, and the defence army would start to fall weak. In the Vaibhajya Dynasty, the army had 4 ranks: soldier, knight, vassal and the king.</p>
+
+            <p>Coming back to one of the warrior families of Ajaynagar, there was the home of Virendra Vritrahanta, a knight. He was a great warrior and had won various battles and sieges. Moreover he was a great father, who trained all his sons quite well. The eldest son, Prajjwal Vritrahanta grew up to be the vassal of Maharaja Har Gobind Singh. Prajjwal Vritrahanta was an extremely powerful and feared warrior, who was also responsible for the administration of Ajaynagar and the neighbouring areas. As a father, he was very strict. He had two sons, Somnath Vritrahanta and Advijay Vritrahanta. Somnath was 17 years old and had joined the army, and showed quite remarkable skills and strength as a warrior. Advijay was currently 15 years old, still in his training phase. He would be turning 16 next month, after which he would officially join the army and fight for the dynasty like others in his family. Prajjwal’s wife, Sanjeevika, was a knight during her time in the army, and currently focused on training her sons.</p>
+
+            <p>On a fine evening, Advijay was at the arena in the backyard of his house, training with his bo staff. His mother stood at a distance, supervising her son.</p>
+
+            <p>Sanjeevika: “Okay! that's enough for today”</p>
+
+            <p>Advijay kept his bo staff at the stand, wiped off the sweat from the forehead, and went to touch his mother's feet and have her blessings.</p>
+
+            <p>Sanjeevika: “Bless you! Hope you become a great warrior like your father someday”</p>
+
+            <p>Advijay: “Mother, why do we have to wait till 16 before we join the army. Am I not strong enough right now?”</p>
+
+            <p>Sanjeevika: “You are undoubtedly stronger than most soldiers in Maharaja Har Gobind Singh's army, after all you are my son”</p>
+
+            <p>Advijay: “Then why I haven't been able to join the army yet, mother?”</p>
+
+            <p>Sanjeevika: “Have you seen a different wolf leading the pack after it's leader is injured?”</p>
+
+            <p>Advijay: “No, he absolutely shouldn't. As long as the leader is alive, no one can take his place. That would be disrespecting the leader”</p>
+
+            <p>Sanjeevika: “Same logic applies here, my dear son. If you join the army now, that would be disrespecting the king, who made this rule”</p>
+
+            <p>Advijay: “I get it, mother. I shall keep training, so that when I join the army, nobody dares to step up to me”</p>
+
+            <p>Sanjeevika: “Son, the most fearful warrior is not someone who wishes to be fearful, but someone who has done such deeds for which they can be respected enough”</p>
+
+            <p>Advijay: “What such deeds can I do right now for which I would be respected in the army?”</p>
+
+            <p>Sanjeevika: “Keep doing what you should, without worrying about the result. If your intention is pure, you shall receive what you deserve”</p>
+
+            <p>There was a knock at the door. Somnath and his father entered the house. Advijay bowed down to touch his father's feet.</p>
+
+            <p>Sanjeevika: “What a coincidence today, both my warriors came home at the same time”</p>
+
+            <p>Prajjwal: “I happen to have less duty to perform these days, given that we are not at war at any front. When I reached for the door, I saw Somnath already standing here”</p>
+
+            <p>Somnath: “My shift ended as usual at 6pm”</p>
+
+            <p>Advijay: “I wonder from when would I also enter home along with my brother and father”</p>
+
+            <p>Prajjwal (angrily): “Shouldn’t you be training right now? Instead you are here talking of joining the army? Go away and train!”</p>
+
+            <p>Sanjeevika: “No, I told him to stop. He had been training for quite long today.”</p>
+
+            <p>Prajjwal: “How long? Your brother was fighting off the revolters of Mednigao the whole day. I didn't see you training when he left for his duty?”</p>
+
+            <p>Advijay quietly went back to the arena. Sanjeevika called him back, and he didn't. Somnath said he would like to have his dinner and go to sleep. Sanjeevika went to the kitchen, worried about Advijay. Prajjwal followed Advijay to the arena. As he entered the arena, he saw his son practicing with his Bo staff.</p>
+
+            <p>Prajjwal: “Don’t tell me you have forgotten to hold that staff!”</p>
+
+            <p>Advijay lowered his grip, holding the staff right at the middle.</p>
+
+            <p>Prajjwal: “What are you waiting for? Attack!”</p>
+
+            <p>Advijay moved his staff, piercing the dummy with the back of the staff, then lifting the front side of his staff and giving a slam directly on top of the dummy’s head, and then turned around, slamming the staff at the dummy ‘s chest.</p>
+
+            <p>Prajjwal: “Why are you so slow? Had it not been a dummy, you would be dead by now”</p>
+
+            <p>Advijay jumped high, kicked straight at the side of the neck of the dummy with his shin, and then slammed the staff also at the same place while he turned with momentum he got because of the kick. Prajjwal seemed impressed.</p>
+
+            <p>Prajjwal: “Answer me, do you wish to go on duty like your brother?”</p>
+
+            <p>Advijay: “I do!”</p>
+
+            <p>Prajjwal: “Then pack your stuff. You need to escort a trader to Lokeshwarnagar. He will be leaving tommorow after sunrise”</p>
+
+            <p>Advijay felt a rush of joy coming from within, but he controlled himself. </p>
+
+            <p>Advijay: “Thank you father”</p>
+
+            <p>Prajjwal (leaving) : “Hope you don't disappoint your family on your very first duty.”</p>
+
+            <p>As his father left, and Advijay shouted in a low voice and jumped in the air, and started to play with his staff. Prajjwal hadn't actually left the place. He was half-turned, looking at his son, with a smile on his face.</p>
+        </>
+    )
+
+    return(
+        <Chapter chapterNo="1" chapterName="Vritrahantas" chapter={story}/>
+    )
+}
+
+export default Chapter1
