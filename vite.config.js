f20a72be2c72af203529aa067ef4ba3cf4e072a0
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2025-08-06 17:45:21 +0530

    Fixed Configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/The_Dragon_Knight',
+  base: '/The_Dragon_Knight/',
   plugins: [react()],
 })
